57d6b2944047aa0057833ec224bc04226f683849
wesrer <sayakchatterjee1309@gmail.com>
2020-10-15 12:05:33 -0400

    some progress?

--- Bristle/src/main.rs
+++ Bristle/src/main.rs
@@ -2,16 +2,19 @@ fn main() {
     println!("Hello, world!");
 }
 
-type ParsedTuple<T> = (Option<T>, String);
-type ParserResult<T> = Result<ParsedTuple<T>, String>;
-type Parser<T> = Box<dyn Fn(String) -> ParserResult<T>>;
+type ParsedTuple<T, X> = (T, Vec<X>);
+type ParserResult<T, X> = Option<Result<ParsedTuple<T, X>, String>>;
+type Parser<T, X> = Box<dyn Fn(&Vec<X>) -> ParserResult<T, X>>;
 
 // Takes in two functions
 // Returns a closure which accepts a String, and parses it
 
 // FIXME: the static lifetimes are ugly
 trait ParserCombinator {
-    fn combinator<T: 'static>(t1: Parser<T>, t2: Parser<T>) -> Parser<T>;
+    fn combinator<T: 'static, X: 'static, P: 'static>(t1: P, t2: P) -> Parser<T, X>
+    where
+        P: Fn(&Vec<X>) -> ParserResult<T, X>,
+        X: Clone;
 }
 
 struct Choice {}
@@ -22,18 +25,26 @@ struct Bind {}
 struct Chain {}
 
 impl ParserCombinator for Choice {
-    fn combinator<T: 'static>(t1: Parser<T>, t2: Parser<T>) -> Parser<T> {
-        return Box::new(move |xs: String| match t1(xs) {
-            Ok((None, rest)) => t2(rest),
+    fn combinator<T: 'static, X: 'static, P: 'static>(t1: P, t2: P) -> Parser<T, X>
+    where
+        P: Fn(&Vec<X>) -> ParserResult<T, X>,
+        X: Clone,
+    {
+        return Box::new(move |xs: &Vec<X>| match t1(&xs) {
+            None => t2(&xs),
             p => p, // The other values, either success or failure, we can just propagate up
         });
     }
 }
 
-impl ParserCombinator for Dollar {
-    fn combinator<T: 'static>(t1: Parser<T>, t2: Parser<T>) -> Parser<T> {
-        return Box::new(move |xs: String| match t1(xs) {
-            Ok((None, rest)) => t2(rest),
+impl ParserCombinator for Star {
+    fn combinator<T: 'static, X: 'static, P: 'static>(t1: P, t2: P) -> Parser<T, X>
+    where
+        P: Fn(&Vec<X>) -> ParserResult<T, X>,
+        X: Clone,
+    {
+        return Box::new(move |xs: &Vec<X>| match t1(&xs) {
+            Some(Ok((value, rest))) => t2(&rest),
             p => p, // The other values, either success or failure, we can just propagate up
         });
     }
